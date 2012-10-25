0e50aa119069b740353624e19ed06f1cff688540
johdirr <johannesdirr@me.com>
2012-10-25 16:16:21 +0200

    set up rails app with mongo db. Yo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SlideCloud::Application.config.secret_token = '0d20b1513d5332a39c576a4b0e20cb4b9e8fcc47b7906e7cad7c6cfdcbfefdf7c79cfac5c6acfd93f3c7918494a6c5826e13a36628120fdd9aedef77ac3cf4d9'
