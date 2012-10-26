0ee4e03d463afcd41663aafb8677e66e2bd9f6ea
Martin Fleischer <fleischer.martin@gmx.net>
2012-10-26 04:32:37 +0200

    fixed alert messages from device

--- app/views/layouts/application.rb
+++ app/views/layouts/application.rb
@@ -19,10 +19,20 @@ module Views
 
       def sign_up
         if user_signed_in?
-          link_to "Log out!", destroy_user_session_path, :method => :delete
+          link_to 'Log out!', destroy_user_session_path, :method => :delete
         else
-          link_to "Sign Up!", user_session_path
+          link_to 'Sign Up!', user_session_path
         end
+      end
+
+      def notice_message
+        unless notice.nil?
+          content_tag(:p, notice, :class => 'notice') unless alert.nil?
+        end
+      end
+
+      def alert_message
+          content_tag(:p, alert, :class => 'alert') unless alert.nil?
       end
     
     end
