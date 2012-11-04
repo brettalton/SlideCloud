0fcaaf0d2c979c3fc67698af97c6974f0bb035ba
johdirr <johannesdirr@me.com>
2012-11-04 19:16:48 +0100

    login modal

---
+++ app/views/devise/sessions/new.rb
@@ -0,0 +1,30 @@
+module Views
+  module Devise
+    module Sessions
+      class New < Layouts::Application
+   
+        def test
+          'test'
+        end
+   
+        def form
+          form_for(resource, :as => resource_name, :url => session_path(resource_name)) do |f|
+            f.label :email
+            f.email_field :email
+
+            f.label :password
+            f.password_field :password
+
+            if devise_mapping.rememberable?
+              f.check_box :remember_me
+              f.label :remember_me
+            end
+
+            f.submit "Sign in"
+        end
+   
+        
+      end
+    end
+  end
+end
