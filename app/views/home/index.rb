179cb06b556356b93f964c6f3882d350173490c6
johdirr <johannesdirr@me.com>
2012-11-10 20:31:24 +0100

    new main page structure

--- app/views/home/index.rb
+++ app/views/home/index.rb
@@ -2,6 +2,11 @@ module Views
   module Home
     class Index < Layouts::Application
 
+
+      def login
+        link_to 'Sign In', user_session_path, :class => "btn btn-large btn-primary", :style => "text-align: center;" 
+      end
+
       def logged_in
         if user_signed_in?
           "Gratz You have logged in! Enjoy!"
