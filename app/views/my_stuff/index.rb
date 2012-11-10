ef309696373f7d23b1f14b5b031c9b8453c9489a
johdirr <johannesdirr@me.com>
2012-11-10 22:07:44 +0100

    small changes

--- app/views/my_stuff/index.rb
+++ app/views/my_stuff/index.rb
@@ -2,6 +2,13 @@ module Views
   module MyStuff
     class Index < Layouts::Application
 
+      def sign_out
+        if user_signed_in?
+          link_to 'Sign Out', destroy_user_session_path, :method => :delete 
+        else
+          link_to 'You shouldnt be here! something went wrong :(', user_session_path
+        end
+      end
 
     end
   end
