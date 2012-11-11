c75306179053618e3705e6d32eef04070295f8cd
johdirr <johannesdirr@me.com>
2012-11-11 14:23:26 +0100

    clean up

--- app/controllers/my_stuff_controller.rb
+++ app/controllers/my_stuff_controller.rb
@@ -1,6 +1,7 @@
 class MyStuffController < ApplicationController
   before_filter :authenticate_user!
   def index
-  
+   @graph = Koala::Facebook::GraphAPI.new(current_user.token)
   end
+  
 end
--- app/views/my_stuff/index.rb
+++ app/views/my_stuff/index.rb
@@ -4,11 +4,20 @@ module Views
 
       def sign_out
         if user_signed_in?
-          link_to current_user.email, destroy_user_session_path, :method => :delete 
+          link_to(current_user.email, destroy_user_session_path, :method => :delete) 
         else
           link_to 'You shouldnt be here! something went wrong :(', user_session_path
         end
       end
+      
+      
+      # def fb_profile_picture
+      #        profile = @graph.get_picture("me", {:type => 'small'})
+      #      end
+      #      
+      #      def fb_user_name
+      #        profile = @graph.get_object("me", {:fields => 'first_name'}).first[1]
+      #      end
 
     end
   end
