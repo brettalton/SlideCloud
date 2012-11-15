76119bd58525128ba3381f8c3e28e3f1c1a241d4
Martin Fleischer <fleischer.martin@gmx.net>
2012-11-15 12:30:46 +0100

    added slides

--- app/views/slideshows/edit.rb
+++ app/views/slideshows/edit.rb
@@ -3,7 +3,7 @@ module Views
     class Edit < Layouts::Application
 
     	def create_slide_btn
-    		link_to "Create", slides_create_path(@slideshow.id), :method => :post, :remote => true, :class => "btn"
+    		link_to "Create", slides_create_path(@slideshow.id), :method => :post, :remote => true, :class => "btn icon-search"
     	end
 
     	def slides
