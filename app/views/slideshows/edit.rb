a1cc03a77218a5a0753b26328e9ef07392c1366f
butjar <fleischer.martin@gmx.net>
2013-01-17 14:39:09 +0100

    resolved merge conflict

--- app/views/slideshows/edit.rb
+++ app/views/slideshows/edit.rb
@@ -1,20 +1,6 @@
 module Views
   module Slideshows
     class Edit < Layouts::Application
-<<<<<<< HEAD
-=======
-
-    	def create_slide_btn
-    		link_to "Create new slide", slides_create_path(@slideshow.id), 
-          :method => :post, 
-          :remote => true, 
-          :class => "btn btn-small"
-    	end
-
-      def save_slide_btn
-
-      end
->>>>>>> 55dffe869322012a67e9fb207870adfd2f135a3e
       
     	def images_list
     		@user.images.each
